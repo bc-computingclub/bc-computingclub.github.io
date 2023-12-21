41e4762925a71f0a02b8580799dbd286ffc995e2
Paul Bauer <107049209+Bauer03@users.noreply.github.com>
2023-12-21 18:08:20 -0500

    Continue progress on cards
    Prevent spam-clicks from breaking card navigaion.
    Decrease visibility of cards not in focus.
    Add new transitions to bg-color for cards.

--- out/cards.js
+++ out/cards.js
@@ -1,34 +1,24 @@
-const cardNodeList = document.querySelectorAll(".card");
-let cardArray = Array.from(cardNodeList);
 const cardContainer = document.querySelector(".cards");
 const cardControlNodeList = document.querySelectorAll(".card-btn");
 const buttonArr = Array.from(cardControlNodeList);
-const learnBtn = document.querySelector(".learn-link");
-const practiceBtn = document.querySelector(".practice-link");
-const experimentBtn = document.querySelector(".experiment-link");
-const navLinkArr = [learnBtn, practiceBtn, experimentBtn];
+const navLinkArr = Array.from(document.querySelectorAll(".link"));
+;
+let cardArray = getCardArray();
 let currentCard = cardArray[0];
-console.log(currentCard);
 let isAnimating = false;
 function updateCurrentButton(targetButtonAttr) {
     let targetButton;
     buttonArr.forEach((button) => {
         if (button.getAttribute("card-label") == targetButtonAttr) {
             targetButton = button;
+            targetButton.classList.add("active-card-btn");
         }
-    });
-    buttonArr.forEach((btn) => {
-        btn.classList.remove("active-card-btn");
-    });
-    targetButton.classList.add("active-card-btn");
-    let tempAttr = targetButton.getAttribute("card-label");
-    buttonArr.forEach((button) => {
-        if (button.classList.contains(tempAttr)) {
-            button.classList.add("active");
+        else {
+            button.classList.remove("active-card-btn");
         }
     });
     navLinkArr.forEach((link) => {
-        if (link.getAttribute("card-label") == tempAttr) {
+        if (link.getAttribute("card-label") == targetButtonAttr) {
             link.classList.add("active");
         }
         else {
@@ -42,19 +32,14 @@ cardContainer.addEventListener("click", (event) => {
         return;
     }
     card = event.target.closest(".card");
-    if (card && cardArray.includes(card)) {
-        if (currentCard.getAttribute("card-label") == card.getAttribute("card-label")) {
-            console.log("trying to remove card");
-            removeCard(card);
-        }
+    if (currentCard == card) {
+        removeCard(card);
     }
 });
 function removeCard(card) {
     isAnimating = true;
     card.classList.add("remove-card");
-    card.addEventListener("animationend", () => {
-        isAnimating = false; // this kind of helps prevent spam clicking from breaking the cards
-    });
+    card.addEventListener("animationend", () => { isAnimating = false; });
     let tempCard = card.cloneNode(true);
     tempCard.classList.remove("remove-card", "initial-state", "current-card");
     tempCard.setAttribute("position", "3");
@@ -62,17 +47,17 @@ function removeCard(card) {
         if (event.animationName == "remove-card") {
             card.remove();
         }
-        cardArray = Array.from(document.querySelectorAll(".card"));
+        cardArray = getCardArray();
         cardArray[0].setAttribute("position", "1");
         cardArray[1].classList.remove("initial-state");
         cardArray[1].setAttribute("position", "2");
         cardContainer.append(tempCard);
         currentCard = cardArray[0];
-        addHoverState(currentCard);
+        currentCard.classList.add("current-card");
         updateCurrentButton(currentCard.getAttribute("card-label"));
     });
 }
-function addHoverState(card) {
-    card.classList.add("current-card");
+function getCardArray() {
+    return Array.from(document.querySelectorAll(".card"));
 }
 //# sourceMappingURL=cards.js.map
--- src/cards.ts
+++ src/cards.ts
@@ -1,45 +1,24 @@
-const cardNodeList = document.querySelectorAll<HTMLElement>(".card");
-let cardArray = Array.from(cardNodeList);
-
 const cardContainer = document.querySelector(".cards") as HTMLElement;
 const cardControlNodeList = document.querySelectorAll<HTMLElement>(".card-btn");
 const buttonArr = Array.from(cardControlNodeList);
-
-const learnBtn = document.querySelector(".learn-link") as HTMLElement;
-const practiceBtn = document.querySelector(".practice-link") as HTMLElement;
-const experimentBtn = document.querySelector(".experiment-link") as HTMLElement;
-const navLinkArr = [learnBtn,practiceBtn,experimentBtn];
-
+const navLinkArr = Array.from(document.querySelectorAll<HTMLElement>(".link"));;
+let cardArray = getCardArray();
 let currentCard = cardArray[0];
-console.log(currentCard);
 let isAnimating = false;
 
 function updateCurrentButton(targetButtonAttr:string) {
     let targetButton: HTMLElement;
-    buttonArr.forEach((button) => {
+    buttonArr.forEach((button) => { // updates buttons with color of current card
         if(button.getAttribute("card-label") == targetButtonAttr) {
             targetButton = button;
-        }
+            targetButton.classList.add("active-card-btn")
+        } else { button.classList.remove("active-card-btn"); }
     })
 
-    buttonArr.forEach((btn) => {
-        btn.classList.remove("active-card-btn");
-    })
-    targetButton.classList.add("active-card-btn");
-    
-    let tempAttr = targetButton.getAttribute("card-label");
-    buttonArr.forEach((button) => {
-        if(button.classList.contains(tempAttr)) {
-            button.classList.add("active");
-        }
-    })
-    
-    navLinkArr.forEach((link) => {
-        if(link.getAttribute("card-label") == tempAttr) {
+    navLinkArr.forEach((link) => { // updates nav links with color of current card
+        if(link.getAttribute("card-label") == targetButtonAttr) {
             link.classList.add("active");
-        } else {
-            link.classList.remove("active");
-        }
+        } else { link.classList.remove("active"); }
     })
 }
 
@@ -49,20 +28,16 @@ cardContainer.addEventListener("click", (event) => {
         return;
     }
     card = (event.target as HTMLElement).closest(".card") as  HTMLElement;
-    if (card && cardArray.includes(card)) {
-        if (currentCard.getAttribute("card-label") == card.getAttribute("card-label")) {
-            console.log("trying to remove card")
-            removeCard(card);
-        }
+    if (currentCard == card) {
+        removeCard(card);
     }
 })
 
 function removeCard(card:HTMLElement) {
     isAnimating = true;
     card.classList.add("remove-card");
-    card.addEventListener("animationend", () => {
-        isAnimating = false; // this kind of helps prevent spam clicking from breaking the cards
-    });
+    card.addEventListener("animationend", () => { isAnimating = false; });
+    
     let tempCard = card.cloneNode(true) as HTMLElement;
     tempCard.classList.remove("remove-card","initial-state","current-card");
     tempCard.setAttribute("position","3");
@@ -71,17 +46,17 @@ function removeCard(card:HTMLElement) {
         if (event.animationName == "remove-card") {
             card.remove();
         }
-        cardArray = Array.from(document.querySelectorAll<HTMLElement>(".card"));
+        cardArray = getCardArray();
         cardArray[0].setAttribute("position","1");
         cardArray[1].classList.remove("initial-state")
         cardArray[1].setAttribute("position","2");
         cardContainer.append(tempCard);
         currentCard = cardArray[0];
-        addHoverState(currentCard);
+        currentCard.classList.add("current-card");
         updateCurrentButton(currentCard.getAttribute("card-label"));
     });
 }
 
-function addHoverState(card:HTMLElement) {
-    card.classList.add("current-card")
+function getCardArray() {
+    return Array.from(document.querySelectorAll<HTMLElement>(".card"));
 }
