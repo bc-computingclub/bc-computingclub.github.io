5f9fdab8910008f6fbacfbf7d456629ccdd75eae
Paul Bauer <107049209+Bauer03@users.noreply.github.com>
2024-02-18 15:54:40 -0500

    Revamp Index.html
    Create new homepage for the web app.
    Place greater emphasis on the three main accent colors.
    Number the sections to imply progression and give the user direction on the site.
    Minimal animations/parallax effect
    Todo: make links work

---
+++ src/home.ts
@@ -0,0 +1,87 @@
+const hBody = document.querySelector('.h-body') as HTMLElement;
+const hDesc = document.querySelector('.h-description') as HTMLElement;
+const hBackground = document.querySelector('.h-background') as HTMLElement;
+
+let timelineBtns = document.querySelectorAll('.timeline-marker') as NodeListOf<HTMLElement>;
+let currentFeature: HTMLElement;
+let activeElm: HTMLElement;
+let currentNum: string;
+
+const sections = [
+    {
+        title: `<h1 class="h-title">Learn</h1>`,
+        body:`<span class="h-text">Explore programming concepts through a series of interactive tutorials built from scratch something something</span>`,
+        button: `<button class="h-redirect" href="/learn/lesson/index.html">GET STARTED<span class="material-symbols-outlined">arrow_forward_ios</span></button>`
+    },
+    {
+        title: `<h1 class="h-title">Practice</h1>`,
+        body:`<span class="h-text">Hone your programming skills by participating in challenges curated by Bridgewater College’s Computing club</span>`,
+        button: `<div class="h-redirect-cont"><button class="h-redirect" href="/practice/index.html">VIEW CHALLENGES<span class="material-symbols-outlined">arrow_forward_ios</span></button><a class="h-redirect-featured" href="/practice/index.html">Featured Challenge</a></div>`
+    },
+    {
+        title: `<h1 class="h-title">Experiment</h1>`,
+        body:`<span class="h-text">Head out on your own and create whatever you’d like here - this is filler text idk what to say.</span>`,
+        button: `<button class="h-redirect">GET STARTED<span class="material-symbols-outlined">arrow_forward_ios</span></button>`
+    },
+]
+
+window.onload = () => {
+    setupFeature();
+    createFeature();
+}
+
+function setupFeature() {
+    currentFeature = document.querySelector('.h-timeline');
+    currentNum = currentFeature.getAttribute("section") as string;
+    activeElm = document.querySelector(".active") as HTMLElement;
+
+    currentFeature.addEventListener('mouseover', handleMouseOver);
+    currentFeature.addEventListener('mouseout', handleMouseOut);
+    currentFeature.addEventListener('click', handleClick);
+}
+
+function handleMouseOver(event: Event) {
+    let hovered = event.target as HTMLElement;
+    if (hovered.classList.contains("timeline-marker")) {
+        timelineBtns.forEach((btn) => { btn.classList.remove("active") });
+    }
+}
+
+function handleMouseOut(event: Event) {
+    currentFeature.setAttribute("section", currentNum);
+    hBody.setAttribute("section", currentNum);
+    activeElm.classList.add("active");
+}
+
+function handleClick(event: Event) {
+    let clicked = event.target as HTMLElement;
+    if (clicked.classList.contains("timeline-marker") && clicked != activeElm) {
+        activeElm.classList.remove("active");
+        clicked.classList.add("active");
+        activeElm = clicked;
+        currentNum = clicked.getAttribute("n") as string;
+        currentFeature.setAttribute("section", currentNum);
+
+        createFeature();
+    }
+}
+
+function createFeature() {
+    while(hDesc.firstChild) hDesc.removeChild(hDesc.firstChild);
+    let temp = document.createElement('div');
+    temp.className = "h-feature active";
+    temp.innerHTML += sections[parseInt(currentNum) - 1].title;
+    temp.innerHTML += sections[parseInt(currentNum) - 1].body;
+    temp.innerHTML += sections[parseInt(currentNum) - 1].button;
+    hDesc.appendChild(temp);
+    hBody.setAttribute("section", currentNum);
+    hBackground.setAttribute("section", currentNum);
+}
+
+// subtle parallax effect, feel free to remove if you don't like it Caleb
+window.addEventListener('mousemove', function(e) {
+    var x = e.clientX / window.innerWidth * 8;
+    var y = e.clientY / window.innerHeight * 8;
+
+    hBackground.style.backgroundPosition = x + '% ' + y + '%';
+});
