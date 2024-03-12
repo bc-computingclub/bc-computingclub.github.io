24e020f828c51b636002aefdff5e70f13bf070b1
PaulxT0 <Paulalexisbauer04@gmail.com>
2024-03-11 20:05:24 -0400

    Revamp Submissions
    • Submissions are now displayed on a single line instead of on a box
    • Move SubmissionMenu to util.ts, and corresponding CSS to util.css
    • Change challenge images to have a gray background (both light and dark theme)

--- src/submissions.ts
+++ src/submissions.ts
@@ -61,24 +61,6 @@ function toggleLineCount() {
   });
 }
 
-function getSubmissionElement(submission: Submission): HTMLElement {
-  let subDiv = document.createElement("div");
-  subDiv.className = "s-card";
-  subDiv.innerHTML = `
-    <div class="s-img-div">
-      <img src="${submission.previewURL}" alt="Submission Thumbnail" class="s-img">
-      <span class="s-line-count">${submission.lineCount || "0 lines"}</span>
-    </div>
-    <div class="s-card-info">
-      <div>${submission.sentBy}</div>
-      <button class="s-open-preview" pid="${submission.pid}">
-        Open
-      </button>
-    </div>
-  `;
-  return subDiv;
-}
-
 cDetails.addEventListener("click", async () => {
   await createChallengePopup(currentChallenge); // replace with reference to actual challenge
 });
@@ -93,114 +75,6 @@ async function getSubmission(uid:string,pid:string){
       resolve(data);
     });
   });
-}
-async function createSubmissionMenu(sub: Submission) {
-  console.log("Creating Submission Menu");
-  if (curSubMenu) {
-    curSubMenu.close();
-    console.log("already a menu");
-  }
-  let subData = await getSubmission(sub.uid,sub.pid);
-  curSubMenu = new SubmissionMenu(subData,sub);
-  curSubMenu.load();
-}
-class SubmissionData{
-  p:ProjectMeta;
-  lines:number;
-  lang:string[];
-  date_submitted:string;
-}
-
-let curSubMenu: SubmissionMenu;
-class SubmissionMenu extends Menu {
-  constructor(data: SubmissionData, sub:Submission) {
-    super("Submission Menu");
-    this.data = data;
-    console.log("loaded data",data);
-    this.submission = sub;
-  }
-
-  data:SubmissionData;
-  submission: Submission;
-
-  load() {
-    let p = this.data.p;
-    let url = getPublicProjectURL(this.submission.uid,this.submission.pid);
-
-    super.load();
-    this.menu.innerHTML = `
-      <div class="s-popup">
-        <div class="s-popup-header">
-          <h1 class="s-popup-title">${this.submission.sentBy}'s Submission</h1>
-          <span class="s-popup-close material-symbols-outlined">close</span>
-        </div>
-        <div class="s-popup-body">
-          <div class="s-popup-code">
-            <h2 class="s-popup-code-title">Code</h2>
-            <div class="s-popup-code-contents">${p.name}<br><br>${p.desc}<!--I'm starting to wonder how far I'm willing to take these class names, it's getting bad...--></div>
-          </div>
-          <div class="s-popup-preview">
-            <div class="s-popup-preview-header">
-              <h2 class="s-popup-preview-title">Preview</h2>
-              <div class="s-preview-title-nested">
-                <button class="b-refresh s-b-refresh icon-btn">
-                  <div class="icon-refresh material-symbols-outlined">sync</div>
-                  <div class="label">Refresh</div>
-                </button>
-                <div style="margin-left:auto;gap:10px" class="flx-h flx-al">
-                  <div class="material-symbols-outlined b-open-in-new co-item" co-label="Open in new tab">open_in_new</div>
-                </div>
-              </div>
-            </div>
-            <div class="s-popup-preview-iframe-cont">
-              <iframe src="${url}" class="s-popup-iframe"></iframe>
-            </div>
-            <div class="s-popup-preview-details">
-              <h3 class="s-popup-preview-details-title">Details</h3>
-              <div class="s-popup-preview-details-contents">
-                <div class="s-popup-preview-details-item">
-                  <h4 class="s-popup-preview-details-item-title">Line Count</h4>
-                  <div class="s-popup-preview-details-item-contents">${this.submission.lineCount || 0}</div>
-                </div>
-                <div class="s-popup-preview-details-item">
-                  <h4 class="s-popup-preview-details-item-title">Language(s)</h4>
-                  <div class="s-popup-preview-details-item-contents">${this.data.lang?.join(", ") || "None"}<!--JavaScript--></div>
-                </div>
-                <div class="s-popup-preview-details-item">
-                  <h4 class="s-popup-preview-details-item-title">Date Submitted</h4>
-                  <div class="s-popup-preview-details-item-contents">2021-09-09</div>
-                </div>
-                <div class="s-popup-preview-details-item">
-                  <h4 class="s-popup-preview-details-item-title">Detail Field</h4>
-                  <div class="s-popup-preview-details-item-contents">Detail Contents Test</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    `;
-
-    let b_refresh = this.menu.querySelector(".s-b-refresh") as HTMLButtonElement;
-    let b_openInNew = this.menu.querySelector(".b-open-in-new") as HTMLButtonElement;
-    let icon_refresh = this.menu.querySelector(".icon-refresh") as HTMLElement;
-    let frame = this.menu.querySelector(".s-popup-iframe") as HTMLIFrameElement;
-
-    b_refresh.addEventListener("click",e=>{
-      frame.src = url;
-      icon_refresh.style.rotate = _icRef_state ? "360deg" : "0deg";
-      _icRef_state = !_icRef_state;
-    });
-    b_openInNew.addEventListener("click",e=>{
-      open(frame.src,"_blank");
-    });
-
-    let sPopupClose = document.querySelector(".s-popup-close") as HTMLElement;
-    sPopupClose.onclick = () => {
-      this.close();
-    };
-    return this;
-  }
 }
 
 sCheckboxes.forEach((cb:HTMLInputElement) => {
--- src/util.ts
+++ src/util.ts
@@ -2700,14 +2700,8 @@ class ChallengeMenu extends Menu {
         `;
         if (this.c.sub_highlights?.length) {
             for (let i = 0; i < this.c.sub_highlights.length; i++) {
-                document.querySelector(".c-implementations").innerHTML += `
-                <div class="c-implementation" onclick="showSubmissions('${this.c.cID}','true','${this.c.sub_highlights[i].pid}')">
-                    <div class="c-implementation-preview">
-                        <img class="c-implementation-img" src="${this.c.sub_highlights[i].previewURL}" alt="challenge image">
-                    </div>
-                    <div class="c-implementation-credit">${this.c.sub_highlights[i].sentBy}</div>
-                </div>
-                `;
+                let temp = getSubmissionElement(this.c.sub_highlights[i],this.c.cID);
+                document.querySelector(".c-implementations").appendChild(temp);
             }
         } else {
             document.querySelector(".c-implementations").innerHTML =
@@ -2962,3 +2956,181 @@ class InputMenu extends Menu {
         this.close();
     }
 }
+
+function getSubmissionElement(submission: Submission, cid?:string): HTMLElement {
+    let subDiv = document.createElement("div");
+    subDiv.className = "s-card";
+  
+    /* Submitted by */
+    let name = document.createElement("span");
+    name.className = "s-name";
+    name.textContent = submission.sentBy;
+    subDiv.appendChild(name);
+  
+    /* Right hand container */
+    let submissionRight = document.createElement("div");
+    submissionRight.className = "s-card-right";
+    subDiv.appendChild(submissionRight);
+  
+    /* Languages used */
+    // let languages = submission.languages as string[];
+    let languages = ["html","css","js"] as string[];
+    let languageCont = document.createElement("div");
+    languageCont.className = "s-languages";
+    submissionRight.appendChild(languageCont);
+    languages.forEach((lang) => {
+      let temp = document.createElement("span") as HTMLElement;
+      temp.textContent = lang;
+      temp.className = `s-lang-${lang}`;
+      languageCont.appendChild(temp);
+    });
+  
+    /* Character count */
+    let charCount = document.createElement("span");
+    charCount.className = "s-char-count";
+    let charCountSymbol = document.createElement("span");
+    charCountSymbol.className = "material-symbols-outlined char-count-symbol";
+    charCountSymbol.textContent = "tag";
+    let charCountText = document.createElement("span");
+    // charCountText.textContent = submission.charCount || "0";
+    charCountText.textContent = '34,598';
+    charCount.appendChild(charCountSymbol);
+    charCount.appendChild(charCountText);
+    submissionRight.appendChild(charCount);
+  
+    /* Time taken */
+    let timeTaken = document.createElement("span");
+    timeTaken.className = "s-time-taken";
+    let dateSymbol = document.createElement("span");
+    dateSymbol.className = "material-symbols-outlined date-symbol";
+    dateSymbol.textContent = "schedule";
+    let timeTakenText = document.createElement("span");
+    timeTakenText.textContent = "4h";
+    timeTaken.appendChild(dateSymbol);
+    timeTaken.appendChild(timeTakenText);
+    submissionRight.appendChild(timeTaken);
+  
+    /* Open button */
+    let openButton = document.createElement("button");
+    openButton.className = "s-open-preview";
+    openButton.textContent = "Open";
+    openButton.setAttribute("pid", submission.pid);
+    submissionRight.appendChild(openButton);
+    if(cid) {
+        openButton.addEventListener("click", () => {
+            showSubmissions(cid, true, submission.pid);
+        });
+    }
+  
+    /* Return completed submission element */
+    return subDiv;
+  }
+
+async function createSubmissionMenu(sub: Submission) {
+    console.log("Creating Submission Menu");
+    if (curSubMenu) {
+        curSubMenu.close();
+        console.log("already a menu");
+    }
+    let subData = await getSubmission(sub.uid,sub.pid);
+    curSubMenu = new SubmissionMenu(subData,sub);
+    curSubMenu.load();
+}
+  class SubmissionData{
+    p:ProjectMeta;
+    lines:number;
+    lang:string[];
+    date_submitted:string;
+  }
+  
+  let curSubMenu: SubmissionMenu;
+  class SubmissionMenu extends Menu {
+    constructor(data: SubmissionData, sub:Submission) {
+      super("Submission Menu");
+      this.data = data;
+      console.log("loaded data",data);
+      this.submission = sub;
+    }
+  
+    data:SubmissionData;
+    submission: Submission;
+  
+    load() {
+      let p = this.data.p;
+      let url = getPublicProjectURL(this.submission.uid,this.submission.pid);
+  
+      super.load();
+      this.menu.innerHTML = `
+        <div class="s-popup">
+          <div class="s-popup-header">
+            <h1 class="s-popup-title">${this.submission.sentBy}'s Submission</h1>
+            <span class="s-popup-close material-symbols-outlined">close</span>
+          </div>
+          <div class="s-popup-body">
+            <div class="s-popup-code">
+              <h2 class="s-popup-code-title">Code</h2>
+              <div class="s-popup-code-contents">${p.name}<br><br>${p.desc}<!--I'm starting to wonder how far I'm willing to take these class names, it's getting bad...--></div>
+            </div>
+            <div class="s-popup-preview">
+              <div class="s-popup-preview-header">
+                <h2 class="s-popup-preview-title">Preview</h2>
+                <div class="s-preview-title-nested">
+                  <button class="b-refresh s-b-refresh icon-btn">
+                    <div class="icon-refresh material-symbols-outlined">sync</div>
+                    <div class="label">Refresh</div>
+                  </button>
+                  <div style="margin-left:auto;gap:10px" class="flx-h flx-al">
+                    <div class="material-symbols-outlined b-open-in-new co-item" co-label="Open in new tab">open_in_new</div>
+                  </div>
+                </div>
+              </div>
+              <div class="s-popup-preview-iframe-cont">
+                <iframe src="${url}" class="s-popup-iframe"></iframe>
+              </div>
+              <div class="s-popup-preview-details">
+                <h3 class="s-popup-preview-details-title">Details</h3>
+                <div class="s-popup-preview-details-contents">
+                  <div class="s-popup-preview-details-item">
+                    <h4 class="s-popup-preview-details-item-title">Line Count</h4>
+                    <div class="s-popup-preview-details-item-contents">${this.submission.lineCount || 0}</div>
+                  </div>
+                  <div class="s-popup-preview-details-item">
+                    <h4 class="s-popup-preview-details-item-title">Language(s)</h4>
+                    <div class="s-popup-preview-details-item-contents">${this.data.lang?.join(", ") || "None"}<!--JavaScript--></div>
+                  </div>
+                  <div class="s-popup-preview-details-item">
+                    <h4 class="s-popup-preview-details-item-title">Date Submitted</h4>
+                    <div class="s-popup-preview-details-item-contents">2021-09-09</div>
+                  </div>
+                  <div class="s-popup-preview-details-item">
+                    <h4 class="s-popup-preview-details-item-title">Detail Field</h4>
+                    <div class="s-popup-preview-details-item-contents">Detail Contents Test</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+  
+      let b_refresh = this.menu.querySelector(".s-b-refresh") as HTMLButtonElement;
+      let b_openInNew = this.menu.querySelector(".b-open-in-new") as HTMLButtonElement;
+      let icon_refresh = this.menu.querySelector(".icon-refresh") as HTMLElement;
+      let frame = this.menu.querySelector(".s-popup-iframe") as HTMLIFrameElement;
+  
+      b_refresh.addEventListener("click",e=>{
+        frame.src = url;
+        icon_refresh.style.rotate = _icRef_state ? "360deg" : "0deg";
+        _icRef_state = !_icRef_state;
+      });
+      b_openInNew.addEventListener("click",e=>{
+        open(frame.src,"_blank");
+      });
+  
+      let sPopupClose = document.querySelector(".s-popup-close") as HTMLElement;
+      sPopupClose.onclick = () => {
+        this.close();
+      };
+      return this;
+    }
+  }
