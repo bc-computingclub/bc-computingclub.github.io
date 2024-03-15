d54e6b5913ae196327e13f489ea427e64423c547
Claeb <87375599+turbonobsin@users.noreply.github.com>
2024-03-14 20:42:28 -0400

    prompt serverURL for testing

--- out/pre_init.js
+++ out/pre_init.js
@@ -2,7 +2,7 @@
 // let serverURL = "http://claebcode.top:3000";
 //let serverURL = "http://157.245.10.20:3000" // DigitalOcean URL
 // let serverURL = "https://baboon-tender-enormously.ngrok-free.app";
-let serverURL = "https://f35a756627b76b9448fc1ae249a045f6.serveo.net";
+let serverURL = prompt("server url","https://baboon-tender-enormously.ngrok-free.app");
 let themes = {
     light: {
         style: "light"
