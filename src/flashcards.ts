3d6712ce52328efc3d81afc9024f54d439a84a4d
Paul Bauer <107049209+Bauer03@users.noreply.github.com>
2024-08-20 18:35:40 -0400

    Progress frontend for flashcards
    Check changelog.md for details (8/20/24)

--- src/flashcards.ts
+++ src/flashcards.ts
@@ -9,7 +9,7 @@ type Flashcard = {
 type FlashcardSet = {
     title: string,
     flashcards: Flashcard[],
-    completed: boolean, // note to self: mark set as completed when all flashcards have been completed. 
+    completed: boolean, // toggle to true when all flashcards are completed 
 }
 
 // Example flashcard set for brainstorming/testing purposes. 
@@ -42,19 +42,97 @@ let lessonOneFlashcards: FlashcardSet = {
     ],
 };
 
-
 class FlashcardMenu extends Menu {
-    constructor() {
+    constructor(fSetArr: FlashcardSet[],fSet?: FlashcardSet) {
         super("Flashcards");
+        this.fSetArr = fSetArr;
+        this.fSet = fSet || fSetArr[0]; // default to displaying the first set of flashcards if none are provided.
     }
+
+    fSetArr: FlashcardSet[];
+    fSet: FlashcardSet;
+
+    /*  notes for paul
+
+        replace: f-title with displayed set's title
+        replace: f-card-front with displayed card's question, language, and whether or not it's starred
+        flashcard nav buttons should be disabled if index is at the beginning or end of the array.
+        flashcard nav buttons do not a) shuffle the array, b) change the order of the array, or c) change the "completed" status of the flashcard set. only change index of the displayed flashcard, and update the display accordingly.
+    */
 
     load() {
         super.load();
+        let sets = getFlashcardSets();
+        
+        this.menu.innerHTML = `
+            <div class="f-outer">
+                <div class="f-outer-top">
+                </div>
+                <div class="f-outer-bottom">
+                    <div class="f-sets-cont">
+                        <!-- Names of flashcard sets go here -->
+                    </div>
+                    <div class="f-inner">
+                        <div class="f-inner-top">
+                            <h2 class="f-title">${this.fSet.title}</h2>
+                            <button class="f-close">Close</button>
+                        </div>
+                        <div class="f-inner-middle">
+                            <div class="f-card">
+                                <div class="f-card-inner">
+                                    <div class="f-card-front">
+                                        <!-- Flashcard question goes here -->
+                                    </div>
+                                    <div class="f-card-back">
+                                        <!-- Flashcard answer goes here -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="f-inner-bottom flx-b">
+                            <div class="f-commands flx">
+                                <button class="f-show-answer">Show Answer</button>
+                            </div>
+                            <div class="f-nav flx-c gp">
+                                <button class="f-prev material-symbols-outlined">arrow_forward</button>
+                                <button class="f-next material-symbols-outlined">arrow_back</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
 
-        this.menu.innerHTML = `testing testing 123`;
+        let setsCont = this.menu.querySelector(".f-sets-cont") as HTMLElement;
+        for(let set of this.fSetArr) {
+            let temp = document.createElement("div");
+            temp.className = "f-set";
+            
+            // I think we should make it so that the user can't toggle the "completed" status of a set of flashcards - if they've completed the set, it's marked as complete. 
+            // Otherwise, they have to go through and click "got it" for all flashcards, after which the status is updated.
+            temp.innerHTML = `
+                <div class="f-set-name">${set.title}</div>
+                <div class="f-set-status material-symbols-outlined">${set.completed ? "check_box_outline_blank" : "check_box"}</div>
+            `; 
+            setsCont.appendChild(temp);
+        }
 
         return this;
     }
 }
 
-// new FlashcardMenu().load();
+let curFlashcardMenu: FlashcardMenu;
+
+/**
+    Takes an array of all flashcard sets available to the user, and an optional flashcard set to display by default.
+ */
+function createFlashcardPopup(fSetArr: FlashcardSet[],fSet?: FlashcardSet) {
+    if (curFlashcardMenu) curFlashcardMenu.close();
+    curFlashcardMenu = new FlashcardMenu(fSetArr,fSet);
+    curFlashcardMenu.load();
+}
+
+// implement backend logic here; this is where I would fetch the flashcard sets from the database.
+async function getFlashcardSets() { 
+    return [lessonOneFlashcards,lessonOneFlashcards];
+}
